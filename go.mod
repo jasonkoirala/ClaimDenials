5043b048756ba46d89ae04ecdde417f3fad537d5
jasonkoirala <33828344+jasonkoirala@users.noreply.github.com>
2023-02-16 00:56:33 -0500

    added cors orgin policy

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/aws/aws-sdk-go v1.44.192
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/rs/cors v1.8.3
 )
 
 require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	acr "github.com/jasonkoirala/ClaimDenials/pkg/routes/aws_cognito_routes"
+	"github.com/rs/cors"
 )
 
 func main() {
 	r := chi.NewRouter()
 	acr.RegisterAWSCognitoRoutes(r)
 	port := os.Getenv("PORT")
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+	handler := cors.Default().Handler(r)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), handler))
 }
